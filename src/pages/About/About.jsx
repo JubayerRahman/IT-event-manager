ff3bf02f42fc92397dc9ce18d710f7d02429dc03
JubayerRahman <jubayerr398@gmail.com>
2023-10-08 02:52:24 +0600

    about and contact page are builded

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import logo from "../../imgs/logo.png"
+import ReviewCarousel from '../../Components/Carousel/ReviewCarousel'
+import Aos from 'aos'
+import "aos/dist/aos.css"
+import { Link } from 'react-router-dom'
 
 const About = () => {
+
+  useEffect (()=>{
+    fetch("./ReviewData.json")
+    .then(res => res.json())
+    .then(data => setReviews(data))
+  },[])
+
   return (
     <div>
-      <h1>About Page</h1>
+      <div className="hero min-h-screen bg-bannerBg rounded-md">
+      <div className="hero-content text-center ">
+        <div className="">
+          <img data-aos="flip-left" className='mx-auto mb-[50px]' src={logo} alt='TechEvent Pros'/>
+          <h1 className=" text-3xl md:text-5xl font-bold">About TechEvent Pros</h1>
+          <div data-aos="fade-right" className='w-[80vw] md:w-1/2 rounded-lg h-[6px] bg-white mx-auto my-[10px]'></div>
+          <p className="py-6 text-white text-xl">TechEvent Pros, led by CEO Jobayer Rahman, is a pioneering IT event management company known for crafting exceptional technology-driven gatherings. With a focus on innovation and precision, Jobayer founded the company to revolutionize conventional events by seamlessly integrating cutting-edge technology. TechEvent Pros is dedicated to curating experiences that blend the latest tech advancements with networking and learning opportunities. As a catalyst for innovation, it stands as a bridge between tech enthusiasts and is committed to redefining event excellence in the digital era, setting the stage for unforgettable and impactful IT events.</p>
+          <Link to="/Contact"><button className="btn bg-[#A937D4] text-white">Get Started</button></Link>
+        </div>
+      </div>
+    </div>
+      <ReviewCarousel/>
     </div>
   )
 }
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,30 @@
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div data-aos="flip-left">
+      <div className="hero min-h-screen bg-bannerBg py-[100px]">
+  <div className="hero-content flex-col lg:flex-row">
+    <div className=" md:w-[40vw] lg:text-left">
+      <h1 className=" text-left text-5xl font-bold text-white">Contact us</h1>
+      <div className='w-[70vw] md:w-1/3 rounded-lg h-[6px] bg-[#A937D4] my-[10px]'></div>
+      <p className="py-6 text-left text-white text-xl">Our Contact Us page at TechEvent Pros ensures seamless communication with our dedicated team. Reach out for inquiries, collaboration, or event solutions. We're here to make your tech events extraordinary.</p>
+      <p title="call us" className="text-white text-xl cursor-pointer"><i className="fa-solid fa-phone-volume mr-[10px]"></i>: 0937935797876634</p>
+      <p title="Send us a mail" className="text-white text-xl cursor-pointer"><i class="fa-solid fa-at mt-[20px] mr-[10px] mb-[50px]"></i>: TechEvent_Pros@mail.com</p>
+    </div>
+    <div className="card flex justify-center md:w-[50vw] shadow-2xl bg-base-100">
+      <form className="p-[20px] lg:card-body">
+        <div className="form-control">
+          <input type="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <textarea className="textarea textarea-bordered h-[200px] mt-[20px] " placeholder="Write your message" required></textarea>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn bg-[#A937D4] hover:bg-[#A937D4] text-white">Send</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
